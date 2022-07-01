695297d5bb9a402bcc48147a382b46ddc78d5c60
Kev Carmichael <kev.carmichael22@gmail.com>
2022-07-01 08:03:37 +0100

    added end screen buttons and username input form

--- game.js
+++ game.js
@@ -52,6 +52,7 @@ startGame = () => {
 getNewQuestion = () => {
     if (availableQuestions.length == 0 || questionCounter >= MAX_QUESTIONS) {
         //need above because the available questions may be more than no of questions you've set 
+        localStorage.setItem("mostRecentScore", score);
         return window.location.assign("/end.html");
     }
 
