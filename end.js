63d2d3b5526ce362e11d20bc5ab9c9d2faca13d6
Kev Carmichael <kev.carmichael22@gmail.com>
2022-07-01 10:46:50 +0100

    end.js save local storage for high scores

--- end.js
+++ end.js
@@ -1,8 +1,12 @@
-
 const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+//need || [] in case first time played, otherwise return null
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+const MAX_HIGH_SCORES = 5;
+
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
@@ -11,4 +15,23 @@ username.addEventListener('keyup', () => {
 
 saveHighScore = (e) => {
     e.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    };
+    
+    highScores.push(score);
+
+    highScores.sort( (a,b) => b.score - a.score);
+
+    highScores.splice(5);
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    //once set high score, return to home screen
+    window.location.assign("/");
+
+    console.log(highScores);
+
 };
+
